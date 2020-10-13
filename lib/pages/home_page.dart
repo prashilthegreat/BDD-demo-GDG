4cc656a63f037f4488af9ce03d9ae30b2d189626
Prashil Koirala <namikazeprashil@gmail.com>
2020-10-13 12:48:55 +0545

    Minor changes in the home page.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,9 +4,18 @@ class HomePage extends StatelessWidget {
   HomePage({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final email = ModalRoute.of(context).settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
         title: Text("HomePage"),
+      ),
+      body: Center(
+        child: Text(
+          email,
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
       ),
     );
   }
